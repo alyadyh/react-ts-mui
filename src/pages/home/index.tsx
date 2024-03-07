9eebbd042880208a0afb0d0dbea6048b07e84c1c
Alya Dhiya <alya.mardhiyyah@binus.ac.id>
2024-03-07 23:59:25 +0700

    home component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useState, SetStateAction, useContext } from "react";
+import Layout from "../../Layout";
+import {
+  Box,
+  Paper,
+  InputBase,
+  InputAdornment,
+  Typography,
+} from "@mui/material";
+import SearchIcon from "../../assets/icons/icon-search.svg";
+import MovieTrendList from "../../components/movie-list/movieTrendList";
+import MovieList from "../../components/movie-list";
+import { MovieDataType } from "../../assets/data";
 
 const Home = () => {
-    return (
-        <div>
-            <div>Home</div>
-        </div>
-    );
-}
+  const [search, setSearch] = useState("");
+  const [searchList, setSearchList] = useState<MovieDataType[]>([]);
+
+  const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <Layout>
+      <Box>
+        <Paper
+          component="form"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "default",
+            p: 1,
+            backgroundColor: "#10141f",
+            border: "none",
+          }}
+        >
+          <InputBase
+            placeholder="Search for movies or TV series"
+            sx={{
+              ml: 1,
+              flex: 1,
+              color: "white",
+              border: "none",
+            }}
+            value={search}
+            onChange={handleSearch}
+            startAdornment={
+              <InputAdornment position="start">
+                <img
+                  src={SearchIcon}
+                  alt="search icon"
+                  width={20}
+                  height={20}
+                />
+              </InputAdornment>
+            }
+          />
+        </Paper>
+      </Box>
+      <Box py={2} px={4}>
+        {search === "" ? (
+          <Box width="100%">
+            <Box width="100%">
+              <Typography variant="h5" component="h1" my={6} fontWeight={400}>
+                Trending
+              </Typography>
+            </Box>
+            <Box width="100%">
+              <Typography variant="h5" component="h1" my={6} fontWeight={400}>
+                Recommended For You
+              </Typography>
+            </Box>
+          </Box>
+        ) : (
+          <Box width="100%">
+            <Typography>
+              Found {searchList.length} results for "{search}"{""}
+            </Typography>
+            <MovieList recommendList={searchList} />
+          </Box>
+        )}
+      </Box>
+    </Layout>
+  );
+};
 
 export default Home;
